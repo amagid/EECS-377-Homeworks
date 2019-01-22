b5099d17a341ac7bddcbeeea1c364212fdc84132
Aaron Magid <ahm64@case.edu>
2019-01-22 19:07:02 +0000

    feat(HW2): Last commit before actual assignment.

--- hw2/hello_app.py
+++ hw2/hello_app.py
@@ -1,4 +1,22 @@
 from kivy.app import App
+from kivy.properties import StringProperty, NumericProperty
+
 
 class HelloApp(App):
-    pass
+
+    formula_text = StringProperty()
+    factor1 = NumericProperty(0)
+    factor2 = NumericProperty(0)
+
+    def on_start(self):
+        self._update_formula_text()
+
+    def on_factor1(self, instance, value):
+        self._update_formula_text()
+
+    def on_factor2(self, instance, value):
+        self._update_formula_text()
+
+    def _update_formula_text(self):
+        product = self.factor1 * self.factor2
+        self.formula_text = "{} x {} = {}".format(self.factor1, self.factor2, product)
